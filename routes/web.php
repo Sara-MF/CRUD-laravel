31d08d52a732b5a26894c8b0746f6b094c25df94
Sara-MF <sara.melo@aluno.uece.br>
2023-09-17 11:24:35 -0300

    test breeze auth working

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BooksController;
 
@@ -15,7 +16,7 @@ use App\Http\Controllers\BooksController;
 */
 
 Route::get('/', function () {
-    return view('master');
+    return view('welcome');
 });
 
 Route::get('/list', [BooksController::class, 'index'])->name('list');
@@ -29,3 +30,15 @@ Route::get('/edit/{id}', [BooksController::class, 'edit'])->name('edit-book');
 Route::put('/edit/{id}', [BooksController::class, 'update'])->name('update-book');
 
 Route::delete('/delete/{id}', [BooksController::class, 'delete'])->name('delete-book');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
